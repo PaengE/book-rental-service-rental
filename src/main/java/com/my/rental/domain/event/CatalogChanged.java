7b18dfd83030a1d2edb2c1836fe2fd54e9fec55f
PaengE <ckddn2820@gmail.com>
2021-07-05 17:57:20 +0900

    refactor: declare @ToString

--- src/main/java/com/my/rental/domain/event/CatalogChanged.java
+++ src/main/java/com/my/rental/domain/event/CatalogChanged.java
@@ -2,7 +2,9 @@ package com.my.rental.domain.event;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Getter
 @Setter
 public class CatalogChanged {
--- src/main/java/com/my/rental/domain/event/StockChanged.java
+++ src/main/java/com/my/rental/domain/event/StockChanged.java
@@ -3,7 +3,9 @@ package com.my.rental.domain.event;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Getter
 @Setter
 @AllArgsConstructor
